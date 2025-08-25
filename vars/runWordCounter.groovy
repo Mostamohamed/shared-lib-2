756d1b53ea8316db4979268d2b1ccf71eaee6121
Mostafa Mohamed <cds.MostafaMohamed13931@alexu.edu.eg>
2025-08-25 16:52:04 +0300

    Update runWordCounter.groovy

--- vars/runWordCounter.groovy
+++ vars/runWordCounter.groovy
@@ -1,6 +1,9 @@
-def call(int n = 1) {
+def call(int n = 1) { 
     sh """
         echo "🔧 Running worker ${n} on master"
+        python3 -m venv .venv || true
+        . .venv/bin/activate
+        pip install -r requirements.txt
         python3 main.py ${n}
     """
 }
