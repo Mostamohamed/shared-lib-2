3e89870430283e0a78f2a3cdeeb2d3637e49dd9a
Mostafa Mohamed <cds.MostafaMohamed13931@alexu.edu.eg>
2025-08-25 16:38:07 +0300

    Update bounds.groovy

--- vars/bounds.groovy
+++ vars/bounds.groovy
@@ -1,3 +1,9 @@
-def call(int x, int y) {
-    echo "Bounds are from ${x} to ${y}"
+def toInt(value, min, max) {
+    try {
+        def intVal = value.toInteger()
+        return Math.max(min, Math.min(max, intVal))
+    } catch(e) {
+        // fallback لو المستخدم كتب قيمة مش رقم
+        return min
+    }
 }
